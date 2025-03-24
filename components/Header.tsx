a214b4cbc31ac0a5d1589b9de5ddc9a4097c529b
Chloe <gtalk.with.nani@gmail.com>
2025-03-24 16:45:08 +0900

    헤더에 메뉴 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Anchor from './Anchor';
 import { useTheme } from './context/ThemeContext';
-import { LogoDark, LogoLight, ModeDark, ModeLight } from './Svgs';
+import { LogoDark, LogoLight, ModeDark, ModeLight, Outlink } from './Svgs';
 import styles from '@/styles/Header.module.sass';
 
 export default function Header() {
@@ -9,10 +9,27 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={`container ${styles.container}`}>
-        <Anchor href="/">
-          {isDarkMode ? <LogoLight /> : <LogoDark />}
-          <span>타임랩</span>
-        </Anchor>
+        <div className={styles.group}>
+          <h1>
+            <Anchor href="/">
+              {isDarkMode ? <LogoLight /> : <LogoDark />}
+              <span>타임랩</span>
+            </Anchor>
+          </h1>
+          <ol>
+            <li>
+              <Anchor href="/converters">
+                <span className={styles.link}>단위 컨버터/계산기</span>
+              </Anchor>
+            </li>
+            <li>
+              <Anchor href="https://timelab.dev1stud.io">
+                <span className={styles.link}>타임랩</span>
+                <Outlink />
+              </Anchor>
+            </li>
+          </ol>
+        </div>
         <button type="button" onClick={toggleTheme}>
           {isDarkMode ? <ModeLight /> : <ModeDark />}
           <span>{isDarkMode ? '라이트 모드로 전환' : '다크 모드로 전환'}</span>
