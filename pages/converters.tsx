bc4236a12084c0f41c339a9d621fbcad99521921
Chloe <gtalk.with.nani@gmail.com>
2025-03-24 16:45:26 +0900

    컨버터 페이지에 백링크 추가

--- pages/converters.tsx
+++ pages/converters.tsx
@@ -1,3 +1,4 @@
+import Anchor from '@/components/Anchor';
 import AreaConverter from '@/components/convertors/Area';
 import LengthConverter from '@/components/convertors/Length';
 import MassConverter from '@/components/convertors/Mass';
@@ -8,12 +9,19 @@ import Supported from '@/components/convertors/Supported';
 import TemperatureConverter from '@/components/convertors/Temperature';
 import TimeBreakdown from '@/components/convertors/TimeBreakdown';
 import VolumeConverter from '@/components/convertors/Volume';
+import { LeftArrow } from '@/components/Svgs';
 import styles from '@/styles/Converters.module.sass';
 
 export default function Converters() {
   return (
     <main className={styles.main}>
       <div className={`container ${styles.container}`}>
+        <div className={styles.backlink}>
+          <Anchor href="/">
+            <LeftArrow />
+            <span>뒤로가기</span>
+          </Anchor>
+        </div>
         <LengthConverter />
         <MassConverter />
         <TemperatureConverter />
